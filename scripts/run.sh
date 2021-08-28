8ee0a8f3a50a5713027d24ea5614c3df913267f6
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-08-28 16:51:00 +0000

    Use physics_benchmarks options file

--- scripts/run.sh
+++ scripts/run.sh
@@ -136,7 +136,7 @@ fi
 
 # Get calibrations (e.g. 'acadia-v1.0-alpha' will pull artifacts from 'acadia')
 if [ ! -d config ] ; then
-  ${RECONSTRUCTION:-/opt/benchmarks/reconstruction_benchmarks}/bin/get_calibrations ${DETECTOR_VERSION/-*/}
+  ${RECONSTRUCTION:-/opt/benchmarks/physics_benchmarks}/bin/get_calibrations ${DETECTOR_VERSION/-*/}
 fi
 
 # Run reconstruction
@@ -144,7 +144,7 @@ export JUGGLER_SIM_FILE="${FULL_FILE}"
 export JUGGLER_REC_FILE="${RECO_FILE}"
 export JUGGLER_N_EVENTS=2147483647
 /usr/bin/time -v \
-  gaudirun.py ${RECONSTRUCTION:-/opt/benchmarks/reconstruction_benchmarks}/benchmarks/full/options/full_reconstruction.py \
+  gaudirun.py ${RECONSTRUCTION:-/opt/benchmarks/physics_benchmarks}/options/reconstruction.py \
   || [ $? -eq 4 ]
 # FIXME why $? = 4
 rootls -t "${RECO_FILE}"

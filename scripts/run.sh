ae9e82ba39f020510520f681b194f0752a449185
Wouter Deconinck <wdconinc@gmail.com>
2021-11-22 21:59:00 +0000

    Update run.sh: --physics.rejectPDGs 1\ 2\ 3\ 4\ 5\ 6\ 21\ 23\ 24\ 25\ 2101

--- scripts/run.sh
+++ scripts/run.sh
@@ -234,7 +234,8 @@ date
   --hepmc3.useHepMC3 ${USEHEPMC3:-true} \
   --compactFile ${DETECTOR_PATH}/${JUGGLER_DETECTOR}.xml \
   --inputFiles ${INPUT_TEMP}/${BASENAME}${EXTENSION} \
-  --outputFile ${FULL_TEMP}/${TASKNAME}.root
+  --outputFile ${FULL_TEMP}/${TASKNAME}.root \
+  --physics.rejectPDGs 1\ 2\ 3\ 4\ 5\ 6\ 21\ 23\ 24\ 25\ 2101
 rm -f ${INPUT_TEMP}/${BASENAME}${EXTENSION}
 ls -al ${FULL_TEMP}/${TASKNAME}.root
 

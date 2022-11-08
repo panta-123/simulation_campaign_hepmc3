5dd03af98d5c3d463c3c28b91161776c273b3ee4
Wouter Deconinck <wdconinc@gmail.com>
2022-11-08 19:44:41 +0000

    feat: jana:warmup_timeout 600 s

--- scripts/run.sh
+++ scripts/run.sh
@@ -265,7 +265,7 @@ fi
 # Run eicrecon reconstruction
 date
 /usr/bin/time -v \
-  run_eicrecon_reco_flags.py "${JUGGLER_SIM_FILE}" "${RECO_TEMP}/${TASKNAME}.eicrecon" -Pjana:warmup_timeout=300 -n=0 -Pjana:timeout=300
+  run_eicrecon_reco_flags.py "${JUGGLER_SIM_FILE}" "${RECO_TEMP}/${TASKNAME}.eicrecon" -Pjana:warmup_timeout=600 -n=0 -Pjana:timeout=300
 
 # Remove full simulation
 rm -f ${FULL_TEMP}/${TASKNAME}.edm4hep.root

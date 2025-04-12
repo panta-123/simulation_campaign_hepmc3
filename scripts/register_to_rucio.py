feb6f9bc2ebc428029b2f0ffdad063718b5c338f
Anil Panta <47672624+panta-123@users.noreply.github.com>
2025-04-12 21:31:26 +0000

    set tombstone for replicas

--- scripts/register_to_rucio.py
+++ scripts/register_to_rucio.py
@@ -3,7 +3,8 @@
 import argparse
 import os
 from rucio.client.uploadclient import UploadClient
-from rucio.common.exception import InputValidationError, RSEWriteBlocked, NoFilesUploaded, NotAllFilesUploaded
+from rucio.client import Client
+from rucio.common.exception import DataIdentifierAlreadyExists, InputValidationError, NoFilesUploaded, NotAllFilesUploaded.
 import logging
 
 parser = argparse.ArgumentParser(prog='Register to RUCIO', description='Registers files to RUCIO')
@@ -30,23 +31,35 @@ upload_items = []  # List to hold the upload items
 # Loop through the file paths and did names (assuming did_names length matches file_paths length)
 for file_path, did_name in zip(file_paths, did_names):
     parent_directory = os.path.dirname(did_name)  # Get the parent directory from did_name
+        # Create a new dictionary for each file and did_name
+        upload_item = {
+            'path': file_path,
+            'rse': rse,
+            'did_scope': scope,
+            'did_name': did_name,
+            'dataset_scope': scope,
+            'dataset_name': parent_directory,
+            'no_register': noregister
+        }
 
-    # Create a new dictionary for each file and did_name
-    upload_item = {
-        'path': file_path,
-        'rse': rse,
-        'did_scope': scope,
-        'did_name': did_name,
-        'dataset_scope': scope,
-        'dataset_name': parent_directory,
-        'no_register': noregister
-    }
-    
     # Append the new item to the upload_items list
     upload_items.append(upload_item)
 
 logger = logging.getLogger('upload_client')
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
+
 upload_client=UploadClient(logger=logger)
-upload_client.upload(upload_items)
+try:
+    upload_client.upload(upload_items, summary_file_path='upload_summary.json')
+except (NoFilesUploaded, NotAllFilesUploaded):
+    # read upload_summary
+    rc_client = Cleint()
+    # check if replicas exits
+    duplicate_did = {'scope': scope, 'name': did_name},
+    replica_info = rc_client.list_replicas([duplicate_did], rse_expression=rse, all_states=True)
+    # check the replica state. If its copying.
+    if replica_info[] == 'C':
+        set_tombstone = rc_client.set_tombstone([{'scope': scope, 'name': did_name, 'rse': rse}])
+
+
